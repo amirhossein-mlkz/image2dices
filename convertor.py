fb2744a00ac9c89f4c082f95683043415d23907b
amirhossein-mlkz <amirhossein.mlkz@gmail.com>
2022-06-08 22:09:38 +0430

    ipny added

--- convertor.py
+++ convertor.py
@@ -11,9 +11,9 @@ for i in range(1,7):
     dice_imgs.append( dimg )
     
 
-img = cv2.imread('img1.jpg', 0 )
+img = cv2.imread('img.jpg', 0 )
 cv2.imshow('image', img )
-img = cv2.resize( img, None, fx=0.25 , fy=0.25 )
+img = cv2.resize( img, None, fx=0.6 , fy=0.6 )
 h,w = img.shape
 dh,dw = dice_imgs[0].shape
 
@@ -25,7 +25,7 @@ criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
 components,label,center=cv2.kmeans( pixels ,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
 center = np.uint8(center)
 
-
+classes = np.arange(0,K)
 x = list(zip(classes, center ))
 x.sort( key = lambda x:x[1])
 
@@ -35,14 +35,15 @@ for i in range(len(x)):
     old_class = x[i][0]
     new_label[ label == old_class ] = i
 
+label_img = new_label.reshape(img.shape)
 result_img = np.zeros( (h*dh , w*dw ), dtype = np.uint8)
 
 for i in range(h):
     for j in range(w):
-        class_color = new_img[i,j]
+        class_color = label_img[i,j]
         result_img[ i*dh : (i+1)*dh , j*dw : (j+1) * dw ] = dice_imgs[class_color] 
         
 
 cv2.imwrite('res.png', result_img )
-cv2.imshow('result_img', cv2.resize( result_img, None, fx=0.47, fy=0.47) )
+cv2.imshow('result_img', cv2.resize( result_img, None, fx=0.8, fy=0.8) )
 cv2.waitKey(0)
